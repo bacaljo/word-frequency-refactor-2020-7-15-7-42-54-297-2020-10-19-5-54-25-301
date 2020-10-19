c57997effe296a0c3c895e5e4b4e67012f0ad775
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 15:37:16 +0800

    [WordFrequencyGame][John] Refactor: Rename Input class to WordFrequency and change field names to word and frequency

---
+++ src/main/java/WordFrequency.java
@@ -0,0 +1,20 @@
+public class WordFrequency {
+    private String word;
+    private int frequency;
+
+    public WordFrequency(String w, int i){
+        this.word =w;
+        this.frequency =i;
+    }
+
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public int getWordCount() {
+        return this.frequency;
+    }
+
+
+}
