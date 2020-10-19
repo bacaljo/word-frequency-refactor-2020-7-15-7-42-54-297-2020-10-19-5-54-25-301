572dbfb415a2a8adf0dfc31309b57714e1842c8a
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 18:55:57 +0800

    [WordFrequencyGame][John] Refactor: Remove unused imports

--- src/test/java/WordFrequencyGameTest.java
+++ src/test/java/WordFrequencyGameTest.java
@@ -1,8 +1,6 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class WordFrequencyGameTest {
 
