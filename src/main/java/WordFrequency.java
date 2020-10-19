4a06612def534661b4912ef02369a425b0a03421
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-19 15:39:37 +0800

    [WordFrequencyGame][John] Refactor: Set WordFrequency fields to final

--- src/main/java/WordFrequency.java
+++ src/main/java/WordFrequency.java
@@ -1,6 +1,6 @@
 public class WordFrequency {
-    private String word;
-    private int frequency;
+    private final String word;
+    private final int frequency;
 
     public WordFrequency(String word, int frequency) {
         this.word = word;
